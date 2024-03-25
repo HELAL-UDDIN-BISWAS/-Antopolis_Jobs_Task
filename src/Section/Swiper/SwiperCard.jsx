7a904fa4937f5900b92ad03569d2bb13b3e28551
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2024-03-25 23:25:37 +0600

    Use Swiper

--- src/Section/Swiper/SwiperCard.jsx
+++ src/Section/Swiper/SwiperCard.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const SwiperCard = () => {
     return (
         <div>
-            swiper
+            <div>
+                <div>
+                    <Swiper
+                        slidesPerView={3}
+                        spaceBetween={30}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        modules={[Pagination]}
+                        className="mySwiper"
+                    ></Swiper>
+
+                    <SwiperSlide>
+                        <img className='rounded h-[500px]' src={'https://i.ibb.co/1Q8Rmfr/pexels-byb-byb-19332231.jpg'} alt="" />
+                        <h2></h2>
+                    </SwiperSlide>
+                </div>
+            </div>
         </div>
     );
 };
